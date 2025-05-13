b38e9d5b17cfbb924ed7a8dbd76c3b3f54a0bab9
xiangnong <xiangnongWu2233@icloud.com>
2025-05-13 13:05:29 -0400

    data_parser.py: fixed green channel extraction

---
+++ data_parser.py
@@ -0,0 +1,45 @@
+from Bio import SeqIO
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+def read_fsa(filename:str):
+    record = SeqIO.read(filename, "abi")
+    signal = record.annotations["abif_raw"]
+    green_channel=np.array(signal.get('DATA4'))
+    orange_channel=np.array(signal.get('DATA205'))
+
+    return green_channel, orange_channel
+
+def plot_signal(signal:np.ndarray,file:str,channel_name:str, save:bool=False, fpath:str=''):
+    plt.figure(figsize=(10,8))
+    plt.plot(signal, color=channel_name)
+    plt.title(f'Electropherogram - {channel_name} Channel - {file}')
+    plt.xlabel('Data Points')
+    plt.ylabel('Fluorescence Intensity')
+    if save:
+        plt.savefig(fpath+f'{file}_{channel_name}.png')
+        plt.close()
+    else:
+        plt.show()
+
+def read_fsa_from_directory(dir):
+    # List all files in the directory
+    files = os.listdir(dir)
+    # Filter for .fsa files
+    fsa_files = [f for f in files if f.endswith('.fsa')]
+    fsa_files.sort()
+    for fsa_file in fsa_files:
+        fsa_path = str(os.path.join(dir, fsa_file))
+        green_channel, orange_channel = read_fsa(fsa_path)
+
+        save_dir='../Results/signal_plots/'
+        save_name=fsa_file.split('.')[0]
+        plot_signal(green_channel,save_name,'green', True, save_dir)
+        plot_signal(orange_channel,save_name,'orange', True, save_dir)
+
+
+
+#file="../Amplicon Length/Test Data/3.17.25 Promega (Redo) UCx #1 03052025/03052025-1A_20250317120227_3-5-25_UCx1_A1_01.fsa"
+data_directory="../Amplicon Length/Test Data/3.17.25 Promega (Redo) UCx #1 03052025/"
+read_fsa_from_directory(data_directory)
---
+++ wave_analysis.py
@@ -0,0 +1,4 @@
+from data_parser import read_fsa
+import numpy as np
+import scipy.signal as signal
+
